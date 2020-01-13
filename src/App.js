05d292f9618bc4e17ad8285219bd5232a7fec46a
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2020-01-13 19:18:04 +0100

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "tui-image-editor/dist/tui-image-editor.css";
+import React from "react";
+import ImageEditor from "@toast-ui/react-image-editor";
+const icona = require("tui-image-editor/dist/svg/icon-a.svg");
+const iconb = require("tui-image-editor/dist/svg/icon-b.svg");
+const iconc = require("tui-image-editor/dist/svg/icon-c.svg");
+const icond = require("tui-image-editor/dist/svg/icon-d.svg");
+const download = require("downloadjs");
+
+const myTheme = {
+  "menu.backgroundColor": "white",
+  "common.backgroundColor": "#151515",
+  "downloadButton.backgroundColor": "white",
+  "downloadButton.borderColor": "white",
+  "downloadButton.color": "black",
+  "menu.normalIcon.path": icond,
+  "menu.activeIcon.path": iconb,
+  "menu.disabledIcon.path": icona,
+  "menu.hoverIcon.path": iconc
+};
 
 function App() {
+  const imageEditor = React.createRef();
+
+  const saveImageToDisk = () => {
+    // const imageEditorInst = imageEditor.current.imageEditorInst;
+    const imageEditorInst = imageEditor.current.getInstance();
+    const data = imageEditorInst.toDataURL();
+    if (data) {
+      const mimeType = data.split(";")[0];
+      const extension = data.split(";")[0].split("/")[1];
+      download(data, `image.${extension}`, mimeType);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ImageEditor
+        includeUI={{
+          loadImage: {
+            path: "adult.jpg",
+            name: "sampleImage"
+          },
+          theme: myTheme,
+          menu: ["crop", "flip", "rotate", "draw", "shape", "text", "filter"],
+          initMenu: "filter",
+          uiSize: {
+            width: "100%",
+            height: `calc(100vh + 160px)`
+          },
+          menuBarPosition: "bottom"
+        }}
+        cssMaxHeight={window.innerHeight}
+        cssMaxWidth={window.innerWidth}
+        selectionStyle={{
+          cornerSize: 20,
+          rotatingPointOffset: 70
+        }}
+        onClick={saveImageToDisk}
+        usageStatistics={true}
+        ref={imageEditor}
+      />
+      {/* <button onClick={saveImageToDisk}>Save</button> */}
+    </>
   );
 }
 
